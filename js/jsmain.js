1f7cec01dd3174828e52a1db2e7dca89a38dcac8
Pedro-Cavenaghi <phenrycavenaghi164@gmail.com>
2026-04-16 03:33:53 -0300

    Navbar, footer e estrutura base finalizados

---
+++ js/jsmain.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuHamburguer = document.querySelector(".menu-hamburguer")
+    const navLinks = document.querySelector(".nav-links")
+
+    if (menuHamburguer && navLinks) {
+        menuHamburguer.addEventListener('click', () => {
+            navLinks.classList.toggle('mobile-menu')
+
+            document.body.classList.toggle('nao-scroll')
+            
+            if (navLinks.classList.contains('mobile-menu')) {
+                menuHamburguer.src = "/assets/icons/cruz-fechar.png"
+            } else {
+                menuHamburguer.src = "/assets/icons/menu-aberto.png"
+            }
+        })
+    }
+})
